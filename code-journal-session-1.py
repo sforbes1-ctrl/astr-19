066f22b71adb305c741be02bceba477bf6d45d75
Shayna <sforbes1@ucsc.edu>
2025-10-16 01:05:38 -0700

    Add files via upload
    Code journal 1

---
+++ code-journal-session-1.py
@@ -0,0 +1,24 @@
+#Code Journal Session 1
+
+
+def Print_Info():
+	# What is my name?
+	print ('Shayna Forbes')
+	# What are my pronouns?
+	print ('My pronouns are She/Her')
+	# What is my Favorite Food?
+	print ('My favorite food any egg dish. I love to eat eggs over easy and soft scrambled')
+	# What is my favorite movie?
+	print ('My favorite movie is Tron: Legacy. I have seen Tron: Legcacy over 100 times and have been watching this movie since I was 6 years old')
+
+
+def main():
+	Print_Info()
+
+
+def another_funciton():
+	print('something else')
+
+
+if __name__=="__main__":
+	main()
