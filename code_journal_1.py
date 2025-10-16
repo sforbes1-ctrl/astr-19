bdfaea8f81527012e2921ac2d146a4bb85ea66d4
Shayna <sforbes1@ucsc.edu>
2025-10-16 00:46:31 -0700

    Delete code_journal_1.py

--- code_journal_1.py
+++
@@ -1,9 +0,0 @@
-Code Journal 1
-# What is my name?
-print('Shayna Forbes')
-# What are my pronouns?
-print('My pronouns are She/Her')
-# What is my Favorite Food?
-print('My favorite food any egg dish. I love to eat eggs over easy and soft scrambled')
-# What is my favorite movie?
-print('My favorite movie is Tron: Legacy. I have seen Tron: Legcacy over 100 times and have been watching this movie since I was 6 years old')
