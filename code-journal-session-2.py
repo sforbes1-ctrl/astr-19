830a67dd093e991fc3519b01c4521cbae563a8d0
Shayna <sforbes1@ucsc.edu>
2025-10-17 00:04:02 -0700

    Add files via upload
    code journal 1 session 2 prompt

---
+++ code-journal-session-2.py
@@ -0,0 +1,50 @@
+#coding journal 1 
+#session 2 prompt
+
+#the differences between two integers
+def main():
+
+	s = "0" #this is our first integer
+	t = "5" #this is our second integer
+	u = "10.0" #the "." declares this as a floating point number
+	#difference between the two integers is 5
+	print(f"{s}, {t}, {u}")
+
+	print("the difference of two integers is 5")
+
+if __name__ == "__main__":
+	main()
+
+#the sum of two floating numbers
+def main():
+	x = "10.0" #floating point number
+	y = "15.0" #second floating point number
+
+	x = input("10.0")
+	y = input("15.0")
+	x + y == input("25.0")
+
+#the sum of 10.0 and 15.0 is 25.0
+	print(f"the sum of floating numbers is",float(10.0) + float(15.0))
+
+if __name__ == "__main__":
+	main()
+
+#the product of a floating number + integer
+def main():
+
+	x = 5 #this is our integer
+	y = 25.0 #this is our floating point number
+
+	x = input("5")
+	y = input("25.0")
+	x + y == input("125.0")
+
+	#the total product is 125.0
+	print(f"the product of a floating number and integer is", 5*25.0)
+
+if __name__ == "__main__":
+	main()
+
+
+
